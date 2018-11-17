97e2ef2d03aec64fe516b449156d958e1134a083
Will H <wandrewhouston@gmail.com>
2018-11-17 16:57:25 -0500

    survey writes to friends.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,4 @@
+const fs = require("fs")
 var friends = require("../data/friends")
 
 module.exports = function(app) {
@@ -5,6 +6,16 @@ module.exports = function(app) {
         res.json(friends)
     })
     app.post("/api/friends", function(req, res) {
-        friends.push(req.body)
+        console.log("Post Recieved")
+        fs.readFile("app/data/friends.js", function(err, data){
+            if (err) throw err
+            var array = JSON.parse(data)
+            array.push(req.body)
+            fs.writeFile("app/data/friends.js", JSON.stringify(array, null, 2), function(err) {
+                if (err) {
+                    return console.log(err)
+                }
+            })
+        })
     })
 }
